2bc3803466c9ad6802a3406bd3c9d9bdee9742c7
Cassandra Le <lenguyetminh1998@gmail.com>
2019-03-24 14:23:18 -0400

    IN PROGRESS: reapply back intro page

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,67 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <!-- <router-view/> -->
+      <div class="container">
+
+          <!-- Display introduction slide here --> 
+          <div v-if="count > 0" class="intro">
+            <Intro @eventname="count = $event"></Intro>
+          </div> 
+      
+          <!-- Star the experiment if count < 0, which means user finished reading the instruction slide -->
+          <!-- <div v-if="count <= 0" class="experiment">
+            {{moveSlides ('blue') }}
+            <OneSlide :oneSlideProp= "oneSlideData"></OneSlide> 
+          </div> -->
+        </div> 
+
   </div>
 </template>
 
-<script>
+<style>
 
-// import OneSlide from ''
-// import Form from '../src/components/form'; 
+body{
+    position: absolute;
+    height: 100%;
+    width: 100%; 
+    background-image: url('http://i.imgur.com/4Pp7dRg.jpg'); 
+    background-size: cover; 
+}
+</style>
+
+<script>
 
 import Form from '../src/components/form';
 import Intro from '../src/components/intro'; 
-// import OneSlide from '../src/components/one-slide';  
-// import Logo from '../../assets/logo.png';
+import OneSlide from '../src/components/one-slide';  
 
 
 
 export default {
-  name: 'App'
+  name: 'App', 
+
+
+  components: {
+      Form, Intro, OneSlide
+    }, 
+
+
+
+   // Return any important data 
+   data() {
+    return {
+      count: 2, 
+      curr_slide: 0, 
+      block: 'blue', 
+
+      // Set experiment data here 
+      oneSlideData : {
+        title: 'Block 1', 
+        block: 'big', 
+      }
+    };
+  }
+
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
